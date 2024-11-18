3bdb0ae61ef3288bddae2ec628da27863c7ae42a
Penny Borelli <penborelli123@gmail.com>
2024-11-18 15:56:02 +0200

    Add project files.

---
+++ StudentPortal.Web/Controllers/StudentsController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using StudentPortal.Web.Data;
+using StudentPortal.Web.Models;
+using StudentPortal.Web.Models.Entities;
+
+namespace StudentPortal.Web.Controllers
+{
+    public class StudentsController : Controller
+    {
+        public ApplicationDbContext _context { get; }
+
+        public StudentsController(ApplicationDbContext context)
+        {
+            this._context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(AddStudentViewModel viewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(viewModel);
+            }
+
+            var student = new Student()
+            {
+                Name = viewModel.Name,
+                Email = viewModel.Email,
+                Phone = viewModel.Phone,
+                Subscribed = viewModel.Subscribed
+            };
+
+            await _context.Students.AddAsync(student);
+            await _context.SaveChangesAsync();
+
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> List()
+        {
+            var students = await _context.Students.OrderBy(q => q.Name).ToListAsync();
+
+            return View(students);
+        }
+    }
+}
---
+++ StudentPortal.Web/Models/AddStudentViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StudentPortal.Web.Models
+{
+    public class AddStudentViewModel
+    {
+        [Required(ErrorMessage = "Name is required!")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Email is required!")]
+        [DataType(DataType.EmailAddress, ErrorMessage = "Email is not valid!")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Phone Number is required!")]
+        public string Phone { get; set; }
+        public bool Subscribed { get; set; }
+    }
+}
